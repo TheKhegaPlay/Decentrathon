aa86e2773f81493228419efea6ea0ea345d9adeb
Владислав <vladik.2015.31@gmail.com>
2023-05-28 01:51:33 +0600

    Create Picocli.java

---
+++ Picocli/Picocli.java
@@ -0,0 +1,10 @@
+package Picocli;
+
+public class Picocli {
+   
+    public static void main(String[] args) {
+           
+        System.out.println("Hello world!");
+        System.out.println("Bye world...");
+    }
+}
